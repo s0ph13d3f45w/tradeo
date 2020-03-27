b1d836b4c7e9df3e328af699e7a923a91c408206
jesuscova <jesuscovam@icloud.com>
2020-03-26 20:53:35 -0500

    working multilanguage with i18next

---
+++ src/components/AppBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import '../i18n'
+import LanguageSelector from '../LanguageSelector'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    fontWeight: 'bold'
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            LOGO♻
+          </Typography>
+          <LanguageSelector />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,12 @@
+const examples = [
+    {
+        name: 'Jesus',
+        email: 'jesuscovam@gmail.com',
+        images: [
+            'https://source.unsplash.com/random',
+            'https://source.unsplash.com/random',
+            'https://source.unsplash.com/random',        
+        ],
+        description: 'Tempor cillum consequat sunt consequat consequat ullamco amet. Laborum pariatur culpa quis nostrud reprehenderit cillum. In do aliqua incididunt nulla. Voluptate Lorem excepteur aute aliquip do et irure laboris officia. Aliquip duis cupidatat enim id voluptate consectetur. Cillum enim sint est esse dolor eiusmod nisi anim magna amet culpa irure aliquip veniam. Anim ad labore labore id id.'
+    }
+]

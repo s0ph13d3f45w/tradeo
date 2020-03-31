a0bf674deec5d4aa5a5316f25858522c59e13b1e
jesuscova <jesuscovam@icloud.com>
2020-03-31 13:23:02 -0500

    added Bigger avatar in UserProfile

--- src/components/ProfileData.js
+++ src/components/ProfileData.js
@@ -3,8 +3,7 @@ import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
 
-const ProfileData = props =>{
-    const {name, img, textSize} = props
+const ProfileData = ({name, img, textSize, imgSize}) =>{
     const useStyles = makeStyles(theme => ({
         root: {
             display: 'flex',
@@ -12,18 +11,33 @@ const ProfileData = props =>{
                 margin: theme.spacing(1),
               },
             marginBottom: '20px',
-            justifyContent: 'center'
-        }
+            justifyContent: 'center',
+        },
+        text:{
+            alignSelf: 'baseline'
+        },
+        small: {
+            width: theme.spacing(5),
+            height: theme.spacing(5),
+          },
+          large: {
+            width: theme.spacing(10),
+            height: theme.spacing(10),
+          }
     }))
     const classes = useStyles()
     return(
         <>
       
         <div className={classes.root}>
-                 <Typography variant={textSize} component="h2">
+                <Typography variant={textSize} component="h2">
                     {name}
                 </Typography>
-                <Avatar alt="profile" src={img}/>
+                {imgSize === 'large'
+                    ? <Avatar alt="profile" src={img} className={classes.large} />
+                    : <Avatar alt="profile" src={img} className={classes.small} />
+                }
+
         <br />
         </div>
         </>
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,27 +1,56 @@
 import React from 'react'
 
-import ProfileData from './ProfileData'
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography'
+import Avatar from '@material-ui/core/Avatar'
+import {makeStyles} from '@material-ui/core/styles'
 
+const useStyles = makeStyles(theme => ({
+    root:{
+        display: 'flex',
+        width:320,
+        flexDirection: 'column',
+        marginTop: 5,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    avatar:{
+        width: theme.spacing(13),
+        height: theme.spacing(13),
+        marginBottom: 5
+    }
+}))
 
-export default function(props){
-    const { user} = props
+const ProfileImage = ({img}) =>{
+    const classes = useStyles()
     return(
-        <>  
-       
-        <div style={{
-            width: 320,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center'}}
-            >
-            <br/>   
-            <ProfileData
-                textSize="h5"
-                name={user.name}
-                img={user.image}
-            />
-          </div>
-        </>
-
+        <Avatar src={img} alt="profile" className={classes.avatar} />
     )
 }
+
+
+const UserProfile = ({user}) =>{
+    const classes = useStyles()
+    return(
+        <Container className={classes.root}>
+            <ProfileImage img={user.image}/>
+            <Typography 
+                variant="h4"
+                component="h2"
+                color="textPrimary">{user.name}</Typography>
+            <div style={{
+                width: '100%',
+                margin: '20, 0',
+                height: 200,
+                backgroundColor: 'tomato',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <h2>Galeria</h2>
+            </div>
+        </Container>
+    )
+}
+
+export default UserProfile

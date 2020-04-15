d333a92a640e13442c6deb3c4ad2a1d76545b00e
jesuscova <jesuscovam@icloud.com>
2020-04-15 11:48:20 -0500

    added first view component for editing profile

---
+++ src/Views/EditProfile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainLayout from '../layouts/MainLayout'
+
+const EditProfile = () =>{
+
+    return(
+        <MainLayout>
+            
+        </MainLayout>
+    )
+}
